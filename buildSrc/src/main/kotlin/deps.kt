98a80ca595e09f16bad2f920c699dac697f306ce
lambdagg <lambdagg@tuta.io>
2022-06-21 13:22:09 +0200

    misc: changed IDEA JDK version from 17 to 11 for more retro-compatibility + added useless files to .idea/.gitignore

---
+++ buildSrc/src/main/kotlin/deps.kt
@@ -0,0 +1,19 @@
+import org.gradle.kotlin.dsl.version
+
+private const val kotlinVersion = "1.7.0"
+
+object Plugins {
+    const val KOTLIN = kotlinVersion
+    const val SEMVER = "0.4.2"
+    const val NEXUS_PUBLISH = "1.0.0"
+}
+
+object Dependencies {
+    const val KOTLIN = kotlinVersion
+
+    val kotlinModules = arrayOf<String>("stdlib")
+}
+
+object Repositories {
+    val mavenUrls = arrayOf<String>()
+}

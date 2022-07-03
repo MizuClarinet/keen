6fc51cbcaf5b0f2ed4f2a9d50c3089cb99f4f133
lambdagg <lambdagg@tuta.io>
2022-07-03 17:04:16 +0200

    dsl: fixed tests

--- kotlin-listener-dsl/src/test/kotlin/wtf/mizu/kawa/dsl/KListenerTest.kt
+++ kotlin-listener-dsl/src/test/kotlin/wtf/mizu/kawa/dsl/KListenerTest.kt
@@ -12,20 +12,27 @@ import wtf.mizu.kawa.OptimizedBus
  * @since 0.0.1
  */
 class KListenerTest {
+    companion object {
+        /**
+         * The expected value.
+         */
+        const val EXPECTED_VALUE = 42
+    }
+
     private val bus = OptimizedBus()
 
     private var i = 0
 
     private val listener = listener {
-        on<String> { i++ }
+        on<Int> { i += it }
     }
 
     @Test
     fun creationTest() {
         bus.addListener(listener)
-        bus.publish(1)
+        bus.publish(EXPECTED_VALUE)
         bus.removeListener(listener)
-        bus.publish(1)
-        assertEquals(1, i)
+        bus.publish(EXPECTED_VALUE)
+        assertEquals(EXPECTED_VALUE, i)
     }
 }

c93cdda6c55de51054cacafb5742cdfee4f7008f
lambdagg <lambdagg@tuta.io>
2022-07-05 10:01:08 +0200

    dsl: gradle: changed dependency order

--- kotlin-listener-dsl/build.gradle.kts
+++ kotlin-listener-dsl/build.gradle.kts
@@ -1,7 +1,8 @@
 dependencies {
-    implementation(project(":api"))
     Dependencies.kotlinModules.forEach {
         implementation("org.jetbrains.kotlin:kotlin-$it:${Dependencies.KOTLIN}")
     }
+    implementation(project(":api"))
+
     testImplementation(project(":optimized-bus"))
 }
